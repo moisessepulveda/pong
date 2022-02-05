2dc8206c121ba9a63ae5dce0fb1817c940f71652
moisessepulveda <mo.sepulvedar@gmail.com>
2022-02-04 22:19:45 -0300

    first commit

---
+++ pong.py
@@ -0,0 +1,18 @@
+import turtle
+
+wn = turtle.Screen()
+
+wn.title("Pong By Moisés Sepúlveda")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# main game loop
+
+# paddle A
+
+
+# paddle B
+
+while True:
+    wn.update()
